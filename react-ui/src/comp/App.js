9ac1c70f82b79aff5fcace615bf4fa9e1e3d0887
Kelsey Steinmann <kelseysteinmann@Kelseys-MacBook-Pro.local>
2018-03-01 15:23:11 -0700

    added okta authentication

--- react-ui/src/comp/App.js
+++ react-ui/src/comp/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
 import Main from './Main.js';
+import LoginPage from './auth/LoginPage.js';
 
 const App = () => (
   <div>
     <Header />
     <Main />
+    <LoginPage />
   </div>
-)
+);
 
 export default App;
--- react-ui/src/comp/Main.js
+++ react-ui/src/comp/Main.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import ApiTest from './ApiTest'
 import BearTest from './BearTest'
+import LoginPage from './auth/LoginPage'
 
 const Main = () => (
   <main>
     <Switch>
+      <Route path = "/login" component = {LoginPage}/>
       <Route exact path='/' component={ApiTest}/>
       <Route exact path='/bears' component={BearTest}/>
     </Switch>
