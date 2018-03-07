d9268f7827a3126e54ec198f790719084fc147b6
Kelsey Steinmann <kelseysteinmann@Kelseys-MacBook-Pro.local>
2018-03-07 10:25:48 -0700

    working on 400 error

--- react-ui/src/comp/auth/.samples.config.js
+++ react-ui/src/comp/auth/.samples.config.js
@@ -1,7 +1,8 @@
 export default {
   oidc: {
     clientId: '0oae6giclrPt2JoAt0h7',
-    issuer: 'https://dev-398658.oktapreview.com/oauth2/default',
+    issuer: 'https://dev-398658.oktapreview.com',
+    // removed (/oauth2/default) from end of issuer
     //Okta can only accept one absolute redirect URI (http://localhost:3000/implicit/callback)
     redirectUri: 'https://enigmatic-coast-39458.herokuapp.com/implicit/callback',
     scope: 'openid profile email',
